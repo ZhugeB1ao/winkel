98fd48b5c0e6c5eb900c8015e31f17666236d0a1
Đoàn Huỳnh Gia Bảo <153264378+ZhugeB1ao@users.noreply.github.com>
2025-11-13 14:07:38 +0700

    update how view component, partial view

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -14,45 +14,77 @@ public class ProductController : Controller
     {
         _context = context;
     }
-    
-    [HttpGet("{slug}")]
-    public IActionResult Index(string slug)
+
+    public IActionResult Index()
     {
-        // Get product info and category that product belong to
-        var product = _context.Products
-            .Include(p => p.Category)
-            .FirstOrDefault(p => p.Slug == slug);
+        return View();
+    }
 
-        if (product == null)
-        {
-            return NotFound();
-        }
-
-        var categories = _context.Categories.ToList();
+    [HttpGet("Detail/{id}")]
+    public IActionResult Detail(int id)
+    {
+        Product product = new Product();
+        product = _context.Products.Find(id);
+        return View(product);
+    }
+    
+    [HttpGet("ListPro/{id}")]
+    public IActionResult ListPro(int id)
+    {
+        // Get child Category
+        var childCategoryIds = _context.Categories
+            .Where(c => c.ParentId == id)
+            .Select(c => c.Id)
+            .ToList();
         
-        // Get parent group 
-        var parent = product.Category.Parent ?? product.Category;
+        childCategoryIds.Add(id);
 
-        // get id from other category in the same group
-        var childIds = parent.Children.Select(c => c.Id).ToList();
-        childIds.Add(parent.Id);
-        
-        // Get related product
-        var relatedProducts = _context.Products
-            .Where(p => childIds.Contains(p.CategoryId) && p.Slug != slug)
-            .Take(4)
+        // Get all Product of current Category or child Category
+        var products = _context.Products
+            .Where(p => childCategoryIds.Contains(p.CategoryId))
             .ToList();
 
-        var view = new ShopView
-        {
-            Categories = categories,
-            Products = relatedProducts,
-            CurrentProduct = product,
-                CurrentCategory = product.Category
-        };
-
-        return View(view);
+        return View("../Shop/Index", products);
     }
+    
+    // [HttpGet("{slug}")]
+    // public IActionResult Index(string slug)
+    // {
+    //     // Get product info and category that product belong to
+    //     var product = _context.Products
+    //         .Include(p => p.Category)
+    //         .FirstOrDefault(p => p.Slug == slug);
+    //
+    //     if (product == null)
+    //     {
+    //         return NotFound();
+    //     }
+    //
+    //     var categories = _context.Categories.ToList();
+    //     
+    //     // Get parent group 
+    //     var parent = product.Category.Parent ?? product.Category;
+    //
+    //     // get id from other category in the same group
+    //     var childIds = parent.Children.Select(c => c.Id).ToList();
+    //     childIds.Add(parent.Id);
+    //     
+    //     // Get related product
+    //     var relatedProducts = _context.Products
+    //         .Where(p => childIds.Contains(p.CategoryId) && p.Slug != slug)
+    //         .Take(4)
+    //         .ToList();
+    //
+    //     var view = new ShopView
+    //     {
+    //         Categories = categories,
+    //         Products = relatedProducts,
+    //         CurrentProduct = product,
+    //             CurrentCategory = product.Category
+    //     };
+    //
+    //     return View(view);
+    // }
     
     
 }
--- WebApplication1/Controllers/ShopController.cs
+++ WebApplication1/Controllers/ShopController.cs
@@ -17,48 +17,57 @@ public class ShopController : Controller
         _context = context;
     }
 
-    [HttpGet("{slug?}")]
-    public IActionResult Index(string? slug)
+    [HttpGet("")]
+    public IActionResult Index()
     {
-        var categories = _context.Categories.ToList();
-
-        if (string.IsNullOrEmpty(slug))
-        {
-            var viewAll = new ShopView
-            {
-                Categories = categories,
-                Products = _context.Products.ToList(),
-                CurrentCategory = null
-            };
-            return View(viewAll);
-        }
-
-        var currentCategory = categories.FirstOrDefault(c => c.Slug == slug);
-
-        if (currentCategory == null)
-        {
-            return NotFound();
-        }
-
-        // Get id of all children category
-        var subCategoryIds = categories
-            .Where(c => c.ParentId == currentCategory.Id)
-            .Select(c => c.Id)
-            .ToList();
-
-        subCategoryIds.Add(currentCategory.Id);
-
-        var products = _context.Products
-            .Where(p => subCategoryIds.Contains(p.CategoryId))
-            .ToList();
-
-        var view = new ShopView
-        {
-            Categories = categories,
-            Products = products,
-            // CurrentCategory = currentCategory
-        };
-
-        return View(view);
+        List<Product> products = _context.Products.ToList();
+        return View(products);
     }
+    
+    
+
+    // [HttpGet("{slug?}")]
+    // public IActionResult Index(string? slug)
+    // {
+    //     var categories = _context.Categories.ToList();
+    //
+    //     if (string.IsNullOrEmpty(slug))
+    //     {
+    //         var viewAll = new ShopView
+    //         {
+    //             Categories = categories,
+    //             Products = _context.Products.ToList(),
+    //             CurrentCategory = null
+    //         };
+    //         return View(viewAll);
+    //     }
+    //
+    //     var currentCategory = categories.FirstOrDefault(c => c.Slug == slug);
+    //
+    //     if (currentCategory == null)
+    //     {
+    //         return NotFound();
+    //     }
+    //
+    //     // Get id of all children category
+    //     var subCategoryIds = categories
+    //         .Where(c => c.ParentId == currentCategory.Id)
+    //         .Select(c => c.Id)
+    //         .ToList();
+    //
+    //     subCategoryIds.Add(currentCategory.Id);
+    //
+    //     var products = _context.Products
+    //         .Where(p => subCategoryIds.Contains(p.CategoryId))
+    //         .ToList();
+    //
+    //     var view = new ShopView
+    //     {
+    //         Categories = categories,
+    //         Products = products,
+    //         // CurrentCategory = currentCategory
+    //     };
+    //
+    //     return View(view);
+    // }
 }
